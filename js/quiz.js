6f809f10d72c8b15710c02a1b6fe2ce684aa0f2d
PravallikaTera <pravallikatera598@gmail.com>
2021-10-22 14:57:20 +0530

    first commit

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Which data type is used to create a variable that should store text?",
+        option:[
+            "1.myString",
+            "2.string",
+            "3.String",
+            "4.Txt",
+        ],
+        answer:3,
+    },
+    {
+        question:"Which character is used  to indicate an end tag?",
+        option:[
+            "1.^",
+            "2.<",
+            "3.*",
+            "4./",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"Which type of JavaScript language is?",
+        option:[
+            "1.Object-Oriented",
+            "2.Object-Based",
+            "3.Assembly-language",
+           "4.High-level",   
+        ],
+        answer:2,
+    },
+    {
+        question:"How to create a checkbox in HTML?",
+        option:[
+            '1.<input type = "checkbox">',
+            '2.<input type = "button">',
+            '3.<checkbox>',
+            '4.<input type = "check">',   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"The property in CSS used to change the background color of an element ",
+        option:[
+            "1.bgcolor",
+            "2.color",
+            "3.background-color",
+            "4.All of the above",   
+        ],
+        answer:3,
+    }
+]
